06cdc6254e024f9cec1600aed5a8951cc2c208f6
wscffaa <1294714904@qq.com>
2020-06-23 09:58:34 +0800

    变量和简单数据类型

--- 2/2.2/Variable.py
+++ 2/2.2/Variable.py
@@ -8,7 +8,7 @@
 MyName = "Hello Python World"
 print(MyName)
 
-#变量命名规则
+#变量命名规则：
 #Python的变量名是区分大小写的
 #变量名只能包含字母、数字和下划线。变量名可以字母或下划线打头，但不能以数字打头，例如，可将变量命名为message_1，但不能将其命名为1_message。
 #变量名不能包含空格，但可使用下划线来分隔其中的单词。例如，变量名greeting_message可行，但变量名greeting message会引发错误。
